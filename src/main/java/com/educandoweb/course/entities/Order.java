5975f8d357e6e47d8449a5d75d93a9bed988004f
Bruno Gago <92176548+BrunoGago@users.noreply.github.com>
2022-12-29 18:42:34 -0300

    OrderStatus ENUM Created

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.educandoweb.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.Entity;
@@ -27,6 +28,9 @@ public class Order implements Serializable{
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
 	
+	//Tendo em vista que o mundo externo irá reportar um número para cada fase do pedido, irei salvar um Integer no BD e nao uma classe
+	private Integer orderStatus;
+	
 	//Definição do relacionamento entre as classes
 	@ManyToOne
 	@JoinColumn(name = "client_id")
@@ -35,10 +39,13 @@ public class Order implements Serializable{
 	public Order() {
 		
 	}
-
-	public Order(Long id, Instant moment, User client) {
+	
+	
+	//No caso do OrderStatus, tendo em vista que estamos acessando a classe, não passei o tipo Integer
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -56,6 +63,16 @@ public class Order implements Serializable{
 
 	public void setMoment(Instant moment) {
 		this.moment = moment;
+	}
+	
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if(orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
 	}
 
 	public User getClient() {
