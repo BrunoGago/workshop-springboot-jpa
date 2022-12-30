dcd8bc3b7656664dbafead4e2ecc89a26c183fc2
Bruno Gago <92176548+BrunoGago@users.noreply.github.com>
2022-12-30 08:13:05 -0300

    OrderItem, many-to-many association with extra attributes

---
+++ src/main/java/com/educandoweb/course/entities/OrderItem.java
@@ -0,0 +1,86 @@
+package com.educandoweb.course.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import com.educandoweb.course.entities.pk.OrderItemPk;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_order_item")
+public class OrderItem implements Serializable{
+	
+
+	private static final long serialVersionUID = 1L;
+	
+	//É uma chave composta, por isso usei o EmbeddedId ao invés de Id
+	@EmbeddedId
+	private OrderItemPk id;
+	private Integer quantity;
+	private Double price;
+	
+	public OrderItem() {
+		
+	}
+
+	//Instância do Pedido e produto, passando a quantidadde e preço
+	public OrderItem(Order order, Product product, Integer quantity, Double price) {
+		id.setOrder(order);
+		id.setProduct(product);
+		this.quantity = quantity;
+		this.price = price;
+	}
+	
+	//tendo em vista que para o mundo exterior o produto e pedido serão mostrados separadamente, um em cada campo, devemos usar os metodos get e set para representá-los
+	public Order getOrder() {
+		return id.getOrder();
+	}
+	
+	public void setOrder(Order order) {
+		id.setOrder(order);
+	}
+	
+	public Product getProduct() {
+		return id.getProduct();
+	}
+	
+	public void setProduct(Product product) {
+		id.setProduct(product);
+	}
+	
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItem other = (OrderItem) obj;
+		return Objects.equals(id, other.id);
+	}	
+}
